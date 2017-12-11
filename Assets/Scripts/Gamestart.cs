f8539f645bf61a2b6dc47dab1c8317121453f90f
mzolfo <mzolfo28@gmail.com>
2017-12-11 17:09:07 -0600

    check

---
+++ Assets/Scripts/Gamestart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Gamestart : MonoBehaviour {
+
+     public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+        Button StartButton = startButton.GetComponent<Button>();
+        StartButton.onClick.AddListener(presstart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void presstart()
+    {
+        SceneManager.LoadScene("demoscene", LoadSceneMode.Single);
+    }
+}
